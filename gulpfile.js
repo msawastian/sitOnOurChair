ac783163d31b046ee9fe0fb2ca2e558a5b7eacee
Mateusz Sawastian <m.sawastian@gmail.com>
2018-05-27 21:51:37 +0200

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var sourcemap = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+    return gulp.src('/scss/main.scss')
+        .pipe(sourcemap.init())
+        .pipe(sass({outputStyle: 'compact'}, {errLogToConsole: true}, {sourceMap: true}))
+        .on('error',function(e){
+            console.error('gulpError', e)
+        })
+        .pipe(sourcemap.write())
+        .pipe(gulp.dest('/css'))
+});
+
+gulp.task('jshint', function () {
+    return gulp.src('script.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('/scss/main.scss', ['sass']);
+});
